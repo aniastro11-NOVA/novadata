8d8e94799086840fe0902d327f66eb2c28433af6
aniastro11-NOVA <aniastro11-NOVA@users.noreply.github.com>
2026-04-20 19:53:02 +0900

    revert: back to /preview URL for iframe

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'novadata-v10';
+const CACHE = 'novadata-v11';
 const ASSETS = ['./', './index.html', './preview.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
