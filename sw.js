f957900dae66622045807450c4ae360b9f112f50
aniastro11-NOVA <aniastro11-NOVA@users.noreply.github.com>
2026-04-20 19:05:05 +0900

    fix: add autoplay=1 and rm=minimal to Drive preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'novadata-v6';
+const CACHE = 'novadata-v7';
 const ASSETS = ['./', './index.html', './preview.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
