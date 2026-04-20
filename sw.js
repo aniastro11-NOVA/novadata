8380cbdcc299cf2086a1ed3ac187f3fd758478f3
aniastro11-NOVA <aniastro11-NOVA@users.noreply.github.com>
2026-04-20 19:51:01 +0900

    fix: switch iframe to /view URL for better centering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'novadata-v9';
+const CACHE = 'novadata-v10';
 const ASSETS = ['./', './index.html', './preview.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
