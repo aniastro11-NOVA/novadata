9ec6dc21552358789468fb26f2b36fe63c40cbc8
aniastro11-NOVA <aniastro11-NOVA@users.noreply.github.com>
2026-05-22 13:34:13 +0900

    chore: bump SW cache to v23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'novadata-v22';
+const CACHE = 'novadata-v23';
 const ASSETS = ['./', './index.html', './preview.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
