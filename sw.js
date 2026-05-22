d01188488ebefafcf0930707f61c082e876b9bc5
aniastro11-NOVA <aniastro11-NOVA@users.noreply.github.com>
2026-05-22 13:28:19 +0900

    chore: bump SW cache to v22 to force update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'novadata-v21';
+const CACHE = 'novadata-v22';
 const ASSETS = ['./', './index.html', './preview.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
